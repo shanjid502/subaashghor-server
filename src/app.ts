f09d25d0eb2ff58ff16f7a53a242d65bdd497428
levi9111 <captainlevi9111@gmail.com>
2026-07-03 19:20:27 +0600

    chore: update application configuration in app.ts

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import config from './app/config';
 const app: Application = express();
 
 // ── Global Middlewares ────────────────────────────────────────────────────────
+app.set('trust proxy', 1);
 app.use(
   helmet({
     crossOriginResourcePolicy: { policy: 'cross-origin' },
